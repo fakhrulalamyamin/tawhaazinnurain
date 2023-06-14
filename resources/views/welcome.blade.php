09c60d7a8fd93bc60a1f85ab70a78a81c7141770
Your Name <you@example.com>
2023-06-14 20:14:25 +0600

    Footer Sectio  Development With Mobile Device

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -490,10 +490,10 @@
         <div class="default-container">
             <div class="register">
                 <div class="register-content">
-                    <h2 class="register-heading">আজই জয়েন করুন বাংলাদেশের সবচেয়ে <br> ব্যতিক্রমধর্মী 
+                    <h2 class="register-heading">আজই জয়েন করুন বাংলাদেশের সবচেয়ে <br> ব্যতিক্রমধর্মী
                         <span class="register-heading-sub">
                             অনলাইন মাদ্রাসায়
-                        </span> 
+                        </span>
                     </h2>
                     <p class="register-span">ফ্রি রেজিস্ট্রেশন করে শেখা শুরু করে দিন</p>
                     <button href="#" class="register-button">রেজিষ্ট্রেশন করুন</button>
@@ -503,6 +503,66 @@
             </div>
         </div>
         <!-- Register End -->
+
+        <!-- Footer Start -->
+        <footer class="footer">
+            <div class="default-container">
+                <div class="footer-content">
+                    <a href="#" class="footer-logo">
+                        <img src="/images/Taw Haa Zin Nurain Logo.png" alt="">
+                    </a>
+
+                    <p class="footer-title">কুরআনের জ্ঞানের খোজে, <br>ত্ব-হা যিন নূরাঈনের আঙিনায় স্বাগতম</p>
+
+                    <div class="footer-contact">
+                        <p>Email: info@tawhaazinnurain.com</p>
+                        <p>Phone: 017 404 33 580</p>
+                    </div>
+                </div>
+
+                <div class="footer-link">
+                    <ul class="footer-list">
+                        <li class="footer-item-heading">কোম্পানী</li>
+                        <li class="footer-item">
+                            <a href="#" class="footer-item-link">আমাদের সম্পর্কে</a>
+                        </li>
+                        <li class="footer-item">
+                            <a href="#" class="footer-item-link">ক্যারিয়ার</a>
+                        </li>
+                        <li class="footer-item">
+                            <a href="#" class="footer-item-link">প্রাইভেসি পলিসি</a>
+                        </li>
+                        <li class="footer-item">
+                            <a href="#" class="footer-item-link">রিফান্ড পলিসি</a>
+                        </li>
+                        <li class="footer-item">
+                            <a href="#" class="footer-item-link">যোগাযোগ</a>
+                        </li>
+                    </ul>
+
+                    <ul class="footer-list">
+                        <li class="footer-item-heading">কোর্সসমূহ</li>
+                        <li class="footer-item">
+                            <a href="#" class="footer-item-link">লার্নিং পথ</a>
+                        </li>
+                        <li class="footer-item">
+                            <a href="#" class="footer-item-link">সকল কোর্স</a>
+                        </li>
+                        <li class="footer-item">
+                            <a href="#" class="footer-item-link">রিসোর্স</a>
+                        </li>
+                        <li class="footer-item">
+                            <a href="#" class="footer-item-link">প্রাসিং</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <hr>
+
+                <p class="footer-copyright">&copy; IQRA Arabic School 2022 | All rights reserved.</p>
+            </div>
+        </footer>
+        <!-- Footer End -->
 
     <!-- Swiper Js -->
     <script src="/js/swiper-bundle.min.js"></script>
