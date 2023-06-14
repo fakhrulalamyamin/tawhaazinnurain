b25c05cd7842a6706679a40ef3241c1326ec4bd6
Your Name <you@example.com>
2023-06-14 22:33:48 +0600

    Footer Section Compleate Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -507,59 +507,67 @@
         <!-- Footer Start -->
         <footer class="footer">
             <div class="default-container">
-                <div class="footer-content">
-                    <a href="#" class="footer-logo">
-                        <img src="/images/Taw Haa Zin Nurain Logo.png" alt="">
-                    </a>
+                <div class="footer-responsive">
+                    <div class="footer-content">
+                        <a href="#" class="footer-logo">
+                            <img src="/images/Taw Haa Zin Nurain Logo.png" alt="">
+                        </a>
 
-                    <p class="footer-title">কুরআনের জ্ঞানের খোজে, <br>ত্ব-হা যিন নূরাঈনের আঙিনায় স্বাগতম</p>
+                        <p class="footer-title">কুরআনের জ্ঞানের খোজে, <br>ত্ব-হা যিন নূরাঈনের আঙিনায় স্বাগতম</p>
 
-                    <div class="footer-contact">
-                        <p>Email: info@tawhaazinnurain.com</p>
-                        <p>Phone: 017 404 33 580</p>
+                        <div class="footer-contact">
+                            <p>Email: info@tawhaazinnurain.com</p>
+                            <p>Phone: 017 404 33 580</p>
+                        </div>
                     </div>
-                </div>
 
-                <div class="footer-link">
-                    <ul class="footer-list">
-                        <li class="footer-item-heading">কোম্পানী</li>
-                        <li class="footer-item">
-                            <a href="#" class="footer-item-link">আমাদের সম্পর্কে</a>
-                        </li>
-                        <li class="footer-item">
-                            <a href="#" class="footer-item-link">ক্যারিয়ার</a>
-                        </li>
-                        <li class="footer-item">
-                            <a href="#" class="footer-item-link">প্রাইভেসি পলিসি</a>
-                        </li>
-                        <li class="footer-item">
-                            <a href="#" class="footer-item-link">রিফান্ড পলিসি</a>
-                        </li>
-                        <li class="footer-item">
-                            <a href="#" class="footer-item-link">যোগাযোগ</a>
-                        </li>
-                    </ul>
+                    <div class="footer-link">
+                        <ul class="footer-list">
+                            <li class="footer-item-heading">কোম্পানী</li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item-link">আমাদের সম্পর্কে</a>
+                            </li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item-link">ক্যারিয়ার</a>
+                            </li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item-link">প্রাইভেসি পলিসি</a>
+                            </li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item-link">রিফান্ড পলিসি</a>
+                            </li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item-link">যোগাযোগ</a>
+                            </li>
+                        </ul>
 
-                    <ul class="footer-list">
-                        <li class="footer-item-heading">কোর্সসমূহ</li>
-                        <li class="footer-item">
-                            <a href="#" class="footer-item-link">লার্নিং পথ</a>
-                        </li>
-                        <li class="footer-item">
-                            <a href="#" class="footer-item-link">সকল কোর্স</a>
-                        </li>
-                        <li class="footer-item">
-                            <a href="#" class="footer-item-link">রিসোর্স</a>
-                        </li>
-                        <li class="footer-item">
-                            <a href="#" class="footer-item-link">প্রাসিং</a>
-                        </li>
-                    </ul>
+                        <ul class="footer-list">
+                            <li class="footer-item-heading">কোর্সসমূহ</li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item-link">লার্নিং পথ</a>
+                            </li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item-link">সকল কোর্স</a>
+                            </li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item-link">রিসোর্স</a>
+                            </li>
+                            <li class="footer-item">
+                                <a href="#" class="footer-item-link">প্রাসিং</a>
+                            </li>
+                        </ul>
+                    </div>
+
                 </div>
 
                 <hr>
 
-                <p class="footer-copyright">&copy; IQRA Arabic School 2022 | All rights reserved.</p>
+                <div class="copyright">
+                    <div>
+                        <h3 class="copyright-heading">&copy; IQRA Arabic School 2022 | All rights reserved.</h3>
+                        <p class="copyright-span">Developed By YEB Ltd.</p>
+                    </div>
+                </div>
             </div>
         </footer>
         <!-- Footer End -->
