1b0f9861b6be0aa81eb0b6b17ac6da434da8739a
Your Name <you@example.com>
2023-06-21 21:29:31 +0600

    Dashboard Navigation Done commit

--- resources/views/components/dashboard-navigation.blade.php
+++ resources/views/components/dashboard-navigation.blade.php
@@ -5,7 +5,9 @@
         </a>
         <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
-            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+            <div class="menu-image">
+                <img src="/public/images/fakhrulAlamYamin.png" alt="Profile Photo" class="menu-img">
+            </div>
         </button>
 
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
@@ -18,7 +20,7 @@
                             </li>
                         @else
                             <li>
-                                <a href="{{route('login')}}" class="nav-link">Login</a>
+                                <a href="{{route('login')}}" class="nav-link">Profile Setting</a>
                             </li>
                             <li>
                                 <a href="{{route('register')}}" class="nav-link">Register</a>
