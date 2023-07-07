a30176b429962f11d8c1e6f9777be7284de54053
Your Name <you@example.com>
2023-07-07 15:10:46 +0600

    Frontend Modules Done

--- resources/views/layouts/larning-path.blade.php
+++ resources/views/layouts/larning-path.blade.php
@@ -5,33 +5,300 @@
                 <h1 class="level-one-title">Level - 01 Contains</h1>
                 <hr class="divider">
 
-                <div class="module">
-                    <div class="module-list">
-                        <h3 class="molute-list-heading">Module 01</h3>
-                        <p class="molute-list-details">(12 Classes/12 Exams/250 mins)</p>
+                <div>
+                    <div>
+                        <div class="module -mb-2">
+                            <div class="module-list">
+                                <h3 class="molute-list-heading">Module 01</h3>
+                                <p class="molute-list-details">(12 Classes/12 Exams/250 mins)</p>
+                            </div>
+                        </div>
+
+                        <div class="w-full" id="accordion-collapse" data-accordion="collapse">
+                            <h2 id="accordion-collapse-heading-1">
+                                <button type="button" class="module-btn" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+                                    <div class="flex">
+                                        @include('components.icons.right-arrow')
+                                        <span class="module-title">আকীদা - আকীদাতুত ত্বহাবী</span>
+                                    </div>
+                                </button>
+                            </h2>
+
+                            <div id="accordion-collapse-body-1" class="hidden ml-8" aria-labelledby="accordion-collapse-heading-1">
+                                <p class="collapse-description">ইসলামি আকীদা সম্পর্কে এই প্লেলিষ্টে আমরা জানব। এখানে ১২ টি ক্লাসে আকীদা ও তাওহীদের বেসিক জ্ঞান অর্জন করতে পারবেন। আকীদা কোর্সটি করার জন্য আকীদাতুত ত্ব-হাবী বইটি প্রয়োজন হবে।</p>
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">আকীদার পরিচয়</p>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">তাওহীদ</p>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">সুন্নাহ</p>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">শিরক</p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
                     </div>
-                </div>
 
-                <div class="w-full" id="accordion-collapse" data-accordion="collapse">
-                    <h2 id="accordion-collapse-heading-1">
-                        <button type="button" class="module-btn" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
-                            <div class="flex">
-                                @include('components.icons.right-arrow')
-                                <span class="module-title">আকীদা - আকীদাতুত ত্বহাবী</span>
+                    <div>
+                        <div class="module -mb-2">
+                            <div class="module-list">
+                                <h3 class="molute-list-heading">Module 02</h3>
+                                <p class="molute-list-details">(12 Classes/12 Exams/250 mins)</p>
                             </div>
-                        </button>
-                    </h2>
+                        </div>
 
-                    <div id="accordion-collapse-body-1" class="hidden ml-8" aria-labelledby="accordion-collapse-heading-1">
-                        <p class="collapse-description">ইসলামি আকীদা সম্পর্কে এই প্লেলিষ্টে আমরা জানব। এখানে ১২ টি ক্লাসে আকীদা ও তাওহীদের বেসিক জ্ঞান অর্জন করতে পারবেন। আকীদা কোর্সটি করার জন্য আকীদাতুত ত্ব-হাবী বইটি প্রয়োজন হবে।</p>
-                        <a href="#">
-                            <div class="collapse-link">
-                                <div class="flex items-center gap-2">
-                                    @include('components.icons.play')
-                                    <p class="collapse-title">আকীদার পরিচয়</p>
-                                </div>
+                        <div class="w-full" id="accordion-collapse" data-accordion="collapse">
+                            <h2 id="accordion-collapse-heading-2">
+                                <button type="button" class="module-btn" data-accordion-target="#accordion-collapse-body-2" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+                                    <div class="flex">
+                                        @include('components.icons.right-arrow')
+                                        <span class="module-title">কুরআন তিলাওয়াত</span>
+                                    </div>
+                                </button>
+                            </h2>
+
+                            <div id="accordion-collapse-body-2" class="hidden ml-8" aria-labelledby="accordion-collapse-heading-2">
+                                <p class="collapse-description">ইসলামি আকীদা সম্পর্কে এই প্লেলিষ্টে আমরা জানব। এখানে ১২ টি ক্লাসে আকীদা ও তাওহীদের বেসিক জ্ঞান অর্জন করতে পারবেন। আকীদা কোর্সটি করার জন্য আকীদাতুত ত্ব-হাবী বইটি প্রয়োজন হবে।</p>
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">আকীদার পরিচয়</p>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">তাওহীদ</p>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">সুন্নাহ</p>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">শিরক</p>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
-                        </a>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="module -mb-2">
+                            <div class="module-list">
+                                <h3 class="molute-list-heading">Module 03</h3>
+                                <p class="molute-list-details">(12 Classes/12 Exams/250 mins)</p>
+                            </div>
+                        </div>
+
+                        <div class="w-full" id="accordion-collapse" data-accordion="collapse">
+                            <h2 id="accordion-collapse-heading-3">
+                                <button type="button" class="module-btn" data-accordion-target="#accordion-collapse-body-3" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+                                    <div class="flex">
+                                        @include('components.icons.right-arrow')
+                                        <span class="module-title">তাফসীর শিক্ষা কোর্স</span>
+                                    </div>
+                                </button>
+                            </h2>
+
+                            <div id="accordion-collapse-body-3" class="hidden ml-8" aria-labelledby="accordion-collapse-heading-3">
+                                <p class="collapse-description">ইসলামি আকীদা সম্পর্কে এই প্লেলিষ্টে আমরা জানব। এখানে ১২ টি ক্লাসে আকীদা ও তাওহীদের বেসিক জ্ঞান অর্জন করতে পারবেন। আকীদা কোর্সটি করার জন্য আকীদাতুত ত্ব-হাবী বইটি প্রয়োজন হবে।</p>
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">আকীদার পরিচয়</p>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">তাওহীদ</p>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">সুন্নাহ</p>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">শিরক</p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="module -mb-2">
+                            <div class="module-list">
+                                <h3 class="molute-list-heading">Module 04</h3>
+                                <p class="molute-list-details">(12 Classes/12 Exams/250 mins)</p>
+                            </div>
+                        </div>
+
+                        <div class="w-full" id="accordion-collapse" data-accordion="collapse">
+                            <h2 id="accordion-collapse-heading-4">
+                                <button type="button" class="module-btn" data-accordion-target="#accordion-collapse-body-4" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+                                    <div class="flex">
+                                        @include('components.icons.right-arrow')
+                                        <span class="module-title">রুকিয়াহ</span>
+                                    </div>
+                                </button>
+                            </h2>
+
+                            <div id="accordion-collapse-body-4" class="hidden ml-8" aria-labelledby="accordion-collapse-heading-4">
+                                <p class="collapse-description">ইসলামি আকীদা সম্পর্কে এই প্লেলিষ্টে আমরা জানব। এখানে ১২ টি ক্লাসে আকীদা ও তাওহীদের বেসিক জ্ঞান অর্জন করতে পারবেন। আকীদা কোর্সটি করার জন্য আকীদাতুত ত্ব-হাবী বইটি প্রয়োজন হবে।</p>
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">আকীদার পরিচয়</p>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">তাওহীদ</p>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">সুন্নাহ</p>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">শিরক</p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="module -mb-2">
+                            <div class="module-list">
+                                <h3 class="molute-list-heading">Module 05</h3>
+                                <p class="molute-list-details">(12 Classes/12 Exams/250 mins)</p>
+                            </div>
+                        </div>
+
+                        <div class="w-full" id="accordion-collapse" data-accordion="collapse">
+                            <h2 id="accordion-collapse-heading-5">
+                                <button type="button" class="module-btn" data-accordion-target="#accordion-collapse-body-5" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+                                    <div class="flex">
+                                        @include('components.icons.right-arrow')
+                                        <span class="module-title">ইসলামের ইতিহাস</span>
+                                    </div>
+                                </button>
+                            </h2>
+
+                            <div id="accordion-collapse-body-5" class="hidden ml-8" aria-labelledby="accordion-collapse-heading-5">
+                                <p class="collapse-description">ইসলামি আকীদা সম্পর্কে এই প্লেলিষ্টে আমরা জানব। এখানে ১২ টি ক্লাসে আকীদা ও তাওহীদের বেসিক জ্ঞান অর্জন করতে পারবেন। আকীদা কোর্সটি করার জন্য আকীদাতুত ত্ব-হাবী বইটি প্রয়োজন হবে।</p>
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">আকীদার পরিচয়</p>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">তাওহীদ</p>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">সুন্নাহ</p>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="collapse-link">
+                                        <div class="flex items-center gap-2">
+                                            @include('components.icons.play')
+                                            <p class="collapse-title">শিরক</p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
